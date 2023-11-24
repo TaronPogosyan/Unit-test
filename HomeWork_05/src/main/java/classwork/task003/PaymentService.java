ca8e13de2f5b6a2d31f58a74c346cdb6c1405e19
TaronPogosyan <109243556+TaronPogosyan@users.noreply.github.com>
2023-11-23 18:05:32 -0600

    Delete HomeWork_05 directory

--- HomeWork_05/src/main/java/classwork/task003/PaymentService.java
+++
@@ -1,8 +0,0 @@
-package classwork.task003;
-
-public class PaymentService {
-    public boolean processPayment(String orderId, double amount) {
-        // Здесь обычно был бы код для обработки платежей, но для примера просто возвращаем true
-        return true;
-    }
-}
